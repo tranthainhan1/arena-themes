f126eb5cfe221586c088c4ca9de8e1abfdcaaab9
Tran Thai Nhan <tranthainhan1996@gmail.com>
2020-08-12 18:12:16 +0700

    modify footer

--- app/scripts/_arn.js
+++ app/scripts/_arn.js
@@ -2,50 +2,17 @@ import { tns } from "tiny-slider/src/tiny-slider";
 
 var AT_main = {
   initTinySlider: (container) => {
+    let config = JSON.parse(
+      container.querySelector("[id*='config-tns']").innerHTML
+    );
     let sliderContainer = container.querySelector(".js-tns");
     let controlsContainer = sliderContainer.nextElementSibling;
-    return tns({
+    config = Object.assign(config, {
       container: sliderContainer,
-      autoWidth: true,
-      loop: false,
-      nav: false,
-      // items: 5,
-      // gutter: 30,
-      controlsContainer: controlsContainer,
-      mouseDrag: true,
-      preventScrollOnTouch: "auto",
-      slideBy: "page",
-      swipeAngle: false,
-      // responsive: {
-      //   1: {
-      //     items: 1,
-      //     gutter: 25,
-      //   },
-      //   375: {
-      //     items: 1.5,
-      //   },
-      //   479: {
-      //     items: 2,
-      //   },
-      //   576: {
-      //     items: 2.5,
-      //   },
-      //   768: {
-      //     items: 3,
-      //   },
-      //   992: {
-      //     items: 3.5,
-      //     gutter: 30,
-      //   },
-      //   1052: {
-      //     items: 4,
-      //     gutter: 30,
-      //   },
-      //   1440: {
-      //     items: 5,
-      //   },
-      // },
+      controlsContainer,
     });
+
+    return tns(config);
   },
 };
 
